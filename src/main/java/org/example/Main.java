f7b4c9cc493890dff32ef95e84984dba5a735cba
subhajit <subhajit.dinda@gmail.com>
2026-07-04 21:19:50 +0530

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+        List list = new ArrayList();   // Raw type
+        list.add(Integer.valueOf(1));
+
+        String s;
+        for (Iterator iter = list.iterator(); iter.hasNext(); ) {
+            s = (String) iter.next();
+            System.out.println(s);
+        }
+    }
+}
---
+++ src/test/java/com/ecample/operator/bitwise/BitwiseOperatorTest.java
@@ -0,0 +1,31 @@
+package com.ecample.operator.bitwise;
+
+import org.junit.jupiter.api.Test;
+
+public class BitwiseOperatorTest {
+
+    //& Bitwise AND
+    //| Bitwise OR
+    //^ Bitwise XOR
+    //~ Bitwise NOT (Complement)
+    //<< Left Shift
+    //>> Signed Right Shift
+    //>>> Unsigned Right Shift
+
+
+
+    //<< Left Shift
+    /**
+     *
+     */
+
+    @Test
+    void TestLeftShift(){
+        int a = 5;
+        int result =  a<< 2;
+        System.out.println("Left Shift Result: " + result);
+    }
+    //>> Signed Right Shift
+    //>>> Unsigned Right Shift
+
+}
